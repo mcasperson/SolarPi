ec6c88a2629d5b044d33991926423c5c1be23782
Matthew Casperson <matthewcasperson@gmail.com>
2018-07-20 16:50:49 +1000

    Started work on controlling the leds

--- src/main/java/com/matthewcasperson/SolarPi.java
+++ src/main/java/com/matthewcasperson/SolarPi.java
@@ -3,12 +3,41 @@ package com.matthewcasperson;
 import com.pi4j.io.gpio.*;
 
 public class SolarPi {
-    public static void main(String[] args) {
+    private final GpioController gpio;
+    private final GpioPinDigitalOutput red;
+    private final GpioPinDigitalOutput yellow;
+    private final GpioPinDigitalOutput green;
+
+    public static void main(final String[] args) {
+        final SolarPi solarPi = new SolarPi();
+    }
+
+    public SolarPi() {
         GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING));
-        final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalOutput red = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_26, "Red", PinState.HIGH);
-        final GpioPinDigitalOutput yellow = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_19, "Yellow", PinState.HIGH);
-        final GpioPinDigitalOutput green = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_13, "Green", PinState.HIGH);
+        gpio = GpioFactory.getInstance();
+        red = getPin(RaspiBcmPin.GPIO_26, "Red");
+        yellow = getPin(RaspiBcmPin.GPIO_19, "Yellow");
+        green = getPin(RaspiBcmPin.GPIO_13, "Green");
+    }
+
+    private void initialLedTest() {
+        red.pulse(1000);
+        yellow.pulse(1000);
+        green.pulse(1000);
+    }
+
+    private GpioPinDigitalOutput getPin(final Pin pin, final String name) {
+        final GpioPinDigitalOutput gpioPin = gpio.provisionDigitalOutputPin(pin, name, PinState.LOW);
+        gpioPin.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+        return gpioPin;
+    }
+
+    private void sleep(final int milliseconds) {
+        try {
+            Thread.sleep(1000);
+        } catch (final InterruptedException e) {
+            // ignored
+        }
     }
 }
 
