ec34bf52d67fe7ca4dfc86e7519380b81d30de10
Matthew Casperson <matthewcasperson@gmail.com>
2018-07-20 20:30:39 +1000

    Ignored some files

--- src/main/java/com/matthewcasperson/SolarPi.java
+++ src/main/java/com/matthewcasperson/SolarPi.java
@@ -24,7 +24,7 @@ public class SolarPi {
      * Anything higher than this value will result in a green display
      */
     private final int MAX_USAGE = 2500;
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm dd-MM-yyyy ");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm dd-MM-yyyy");
     private static final String CURRENT_OUTPUT_ELEMENT = "tr.tr1:nth-child(5) > td:nth-child(2)";
     private static final String SOLAR_USER = "SOLAR_USER";
     private static final String SOLAR_PASS = "SOLAR_PASS";
