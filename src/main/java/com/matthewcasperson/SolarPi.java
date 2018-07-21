5d740dfdf45447235291f2d43818b9b5089dc04b
Matthew Casperson <matthewcasperson@gmail.com>
2018-07-22 09:43:45 +1000

    Reduce the polling frequency

--- src/main/java/com/matthewcasperson/SolarPi.java
+++ src/main/java/com/matthewcasperson/SolarPi.java
@@ -31,7 +31,7 @@ public class SolarPi {
     private static final String SOLAR_USER = "SOLAR_USER";
     private static final String SOLAR_PASS = "SOLAR_PASS";
     private static final String SOLAR_URL = "SOLAR_URL";
-    private static final int REFRESH_PERIOD = 20000;
+    private static final int REFRESH_PERIOD = 60000;
     private static final int INITIAL_TEST_PERIOD = 1000;
     private GpioController gpio;
     private GpioPinDigitalOutput red;
