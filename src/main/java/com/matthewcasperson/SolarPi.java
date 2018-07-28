ffd86f902777c3575cf7ebe0f0bc4837e23b9f22
Matthew Casperson <matthewcasperson@gmail.com>
2018-07-29 09:53:35 +1000

    Still playing with effects

--- src/main/java/com/matthewcasperson/SolarPi.java
+++ src/main/java/com/matthewcasperson/SolarPi.java
@@ -15,7 +15,7 @@ public class SolarPi {
     private static final GeneralUtils GENERAL_UTILS = new GeneralUtilsImpl();
     private static final int REFRESH_PERIOD = 60000;
     private static final int INITIAL_TEST_PERIOD = 1000;
-    private static final long MAX_FRAME_RATE = 500;
+    private static final long MAX_FRAME_RATE = 50;
     private static final Blinkt BLINKT = new BlinktImpl();
     private static final Display[] DISPLAYS = new Display[] {
             new SolarDisplay(),
