644e626b4a5c93faa21a274191eb994bc1c74e02
Matthew Casperson <matthewcasperson@gmail.com>
2018-07-31 15:01:48 +1000

    Added a retry policy to the web calls

--- src/main/java/com/matthewcasperson/datasources/impl/FuelImpl.java
+++ src/main/java/com/matthewcasperson/datasources/impl/FuelImpl.java
@@ -3,33 +3,43 @@ package com.matthewcasperson.datasources.impl;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.matthewcasperson.datasources.Fuel;
+import com.matthewcasperson.utils.Retry;
 import com.matthewcasperson.utils.WebUtils;
+import com.matthewcasperson.utils.impl.RetryImpl;
 import com.matthewcasperson.utils.impl.WebUtilsImpl;
 import org.joda.time.DateTime;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
 
 public class FuelImpl implements Fuel {
     private static final WebUtils WEB_UTILS = new WebUtilsImpl();
+    private static final Retry<Boolean, Exception> RETRY = new RetryImpl<>();
     private static final String URL = "https://api-01-04.motormouth.com.au/GetChartData?appToken=EE242BF3-7D54-4783-A1D9-DA1E0E403F17&userToken=&isFuelId=2&cityId=1";
 
     public boolean isGoodFuelDay() {
         try {
-            final String response = WEB_UTILS.HttpGet(URL);
-            final JsonArray predictions = new JsonParser().parse(response)
-                    .getAsJsonObject()
-                    .getAsJsonArray("Forecast");
+            return RETRY.retry(new RetryCallback<Boolean, Exception>() {
+                @Override
+                public Boolean doWithRetry(RetryContext context) throws Exception {
+                    final String response = WEB_UTILS.HttpGet(URL);
+                    final JsonArray predictions = new JsonParser().parse(response)
+                            .getAsJsonObject()
+                            .getAsJsonArray("Forecast");
 
-            DateTime smallestDate = null;
-            int smallestValue = 0;
-            for (int i = 0; i < predictions.size(); ++i) {
-                final DateTime date = DateTime.parse(predictions.get(i).getAsJsonObject().get("DateTimeLocal").getAsString());
-                if (smallestDate == null || date.isBefore(smallestDate)) {
-                    smallestDate = date;
-                    smallestValue = predictions.get(i).getAsJsonObject().get("Value").getAsInt();
+                    DateTime smallestDate = null;
+                    int smallestValue = 0;
+                    for (int i = 0; i < predictions.size(); ++i) {
+                        final DateTime date = DateTime.parse(predictions.get(i).getAsJsonObject().get("DateTimeLocal").getAsString());
+                        if (smallestDate == null || date.isBefore(smallestDate)) {
+                            smallestDate = date;
+                            smallestValue = predictions.get(i).getAsJsonObject().get("Value").getAsInt();
+                        }
+                    }
+
+                    return smallestValue == 1;
                 }
-            }
-
-            return smallestValue == 1;
-        } catch (final Exception ex) {
+            });
+        } catch (final Throwable ex) {
             System.err.println(ex);
             return false;
         }
--- src/main/java/com/matthewcasperson/datasources/impl/WeatherImpl.java
+++ src/main/java/com/matthewcasperson/datasources/impl/WeatherImpl.java
@@ -3,28 +3,38 @@ package com.matthewcasperson.datasources.impl;
 import com.google.gson.JsonParser;
 import com.matthewcasperson.datasources.Weather;
 import com.matthewcasperson.utils.ConfigurationUtils;
+import com.matthewcasperson.utils.Retry;
 import com.matthewcasperson.utils.WebUtils;
 import com.matthewcasperson.utils.impl.ConfigurationUtilsImpl;
+import com.matthewcasperson.utils.impl.RetryImpl;
 import com.matthewcasperson.utils.impl.WebUtilsImpl;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
 
 public class WeatherImpl implements Weather {
+    private static final Retry<Boolean, Exception> RETRY = new RetryImpl<>();
     private static final ConfigurationUtils CONFIGURATION_UTILS = new ConfigurationUtilsImpl();
     private static final WebUtils WEB_UTILS = new WebUtilsImpl();
 
     public boolean getRainToday() {
         try {
-            final String weatherResponse = WEB_UTILS.HttpGet("https://api.darksky.net/forecast/" +
-                    CONFIGURATION_UTILS.getConfigValue("WEATHER_API_KEY") +
-                    "/27.2015,152.9655");
-            final float rainForecast = new JsonParser().parse(weatherResponse)
-                    .getAsJsonObject()
-                    .getAsJsonArray("daily")
-                    .get(0)
-                    .getAsJsonObject()
-                    .get("precipProbability")
-                    .getAsFloat();
-            return rainForecast >= 0.5;
-        } catch (final Exception ex) {
+            return RETRY.retry(new RetryCallback<Boolean, Exception>() {
+                @Override
+                public Boolean doWithRetry(RetryContext context) throws Exception {
+                    final String weatherResponse = WEB_UTILS.HttpGet("https://api.darksky.net/forecast/" +
+                            CONFIGURATION_UTILS.getConfigValue("WEATHER_API_KEY") +
+                            "/27.2015,152.9655");
+                    final float rainForecast = new JsonParser().parse(weatherResponse)
+                            .getAsJsonObject()
+                            .getAsJsonArray("daily")
+                            .get(0)
+                            .getAsJsonObject()
+                            .get("precipProbability")
+                            .getAsFloat();
+                    return rainForecast >= 0.5;
+                }
+            });
+        } catch (final Throwable ex) {
             System.err.println(ex);
             return false;
         }
